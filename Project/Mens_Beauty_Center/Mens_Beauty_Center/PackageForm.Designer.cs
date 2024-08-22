57f1e8531bfd910319c011b28f4242f0cf157924
Faresahmed9 <faresfouad5550@gmail.com>
2024-08-22 14:28:34 +0200

    Package Update

--- Project/Mens_Beauty_Center/Mens_Beauty_Center/PackageForm.Designer.cs
+++ Project/Mens_Beauty_Center/Mens_Beauty_Center/PackageForm.Designer.cs
@@ -31,19 +31,20 @@ namespace Mens_Beauty_Center
         private void InitializeComponent()
         {
             this.dataGridView1 = new System.Windows.Forms.DataGridView();
-            this.PackageName = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.Description = new System.Windows.Forms.DataGridViewTextBoxColumn();
-            this.TotalPrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.AddBtn = new System.Windows.Forms.Button();
+            this.UpdateBtn = new System.Windows.Forms.Button();
+            this.DeleteBtn = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.PackageNameTxt = new System.Windows.Forms.TextBox();
+            this.DescriptionTxt = new System.Windows.Forms.RichTextBox();
+            this.PriceTxt = new System.Windows.Forms.TextBox();
+            this.Package_ID = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Package_Name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PackageDescription = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.PackagePrice = new System.Windows.Forms.DataGridViewTextBoxColumn();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -53,28 +54,14 @@ namespace Mens_Beauty_Center
             this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
-            this.PackageName,
-            this.Description,
-            this.TotalPrice});
+            this.Package_ID,
+            this.Package_Name,
+            this.PackageDescription,
+            this.PackagePrice});
             this.dataGridView1.Location = new System.Drawing.Point(437, 65);
             this.dataGridView1.Name = "dataGridView1";
             this.dataGridView1.Size = new System.Drawing.Size(432, 332);
             this.dataGridView1.TabIndex = 0;
-            // 
-            // PackageName
-            // 
-            this.PackageName.HeaderText = "اسم البكج";
-            this.PackageName.Name = "PackageName";
-            // 
-            // Description
-            // 
-            this.Description.HeaderText = "محتويات البكج";
-            this.Description.Name = "Description";
-            // 
-            // TotalPrice
-            // 
-            this.TotalPrice.HeaderText = "سعر البكج";
-            this.TotalPrice.Name = "TotalPrice";
             // 
             // label1
             // 
@@ -86,50 +73,53 @@ namespace Mens_Beauty_Center
             this.label1.TabIndex = 1;
             this.label1.Text = "بكج العريس";
             // 
-            // button1
+            // AddBtn
             // 
-            this.button1.BackColor = System.Drawing.Color.DarkGreen;
-            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button1.FlatAppearance.BorderSize = 0;
-            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button1.Location = new System.Drawing.Point(220, 414);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 30);
-            this.button1.TabIndex = 4;
-            this.button1.Text = "اضافه";
-            this.button1.UseVisualStyleBackColor = false;
+            this.AddBtn.BackColor = System.Drawing.Color.DarkGreen;
+            this.AddBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.AddBtn.FlatAppearance.BorderSize = 0;
+            this.AddBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.AddBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.AddBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.AddBtn.Location = new System.Drawing.Point(220, 414);
+            this.AddBtn.Name = "AddBtn";
+            this.AddBtn.Size = new System.Drawing.Size(75, 30);
+            this.AddBtn.TabIndex = 4;
+            this.AddBtn.Text = "اضافه";
+            this.AddBtn.UseVisualStyleBackColor = false;
+            this.AddBtn.Click += new System.EventHandler(this.AddBtn_Click);
             // 
-            // button2
+            // UpdateBtn
             // 
-            this.button2.BackColor = System.Drawing.Color.DarkBlue;
-            this.button2.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button2.FlatAppearance.BorderSize = 0;
-            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button2.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button2.Location = new System.Drawing.Point(118, 414);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 30);
-            this.button2.TabIndex = 4;
-            this.button2.Text = "تعديل";
-            this.button2.UseVisualStyleBackColor = false;
+            this.UpdateBtn.BackColor = System.Drawing.Color.DarkBlue;
+            this.UpdateBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.UpdateBtn.FlatAppearance.BorderSize = 0;
+            this.UpdateBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.UpdateBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UpdateBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.UpdateBtn.Location = new System.Drawing.Point(118, 414);
+            this.UpdateBtn.Name = "UpdateBtn";
+            this.UpdateBtn.Size = new System.Drawing.Size(75, 30);
+            this.UpdateBtn.TabIndex = 4;
+            this.UpdateBtn.Text = "تعديل";
+            this.UpdateBtn.UseVisualStyleBackColor = false;
+            this.UpdateBtn.Click += new System.EventHandler(this.UpdateBtn_Click);
             // 
-            // button3
+            // DeleteBtn
             // 
-            this.button3.BackColor = System.Drawing.Color.DarkRed;
-            this.button3.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.button3.FlatAppearance.BorderSize = 0;
-            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.button3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button3.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
-            this.button3.Location = new System.Drawing.Point(25, 414);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 30);
-            this.button3.TabIndex = 4;
-            this.button3.Text = "مسح";
-            this.button3.UseVisualStyleBackColor = false;
+            this.DeleteBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.DeleteBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DeleteBtn.FlatAppearance.BorderSize = 0;
+            this.DeleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DeleteBtn.Location = new System.Drawing.Point(25, 414);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(75, 30);
+            this.DeleteBtn.TabIndex = 4;
+            this.DeleteBtn.Text = "مسح";
+            this.DeleteBtn.UseVisualStyleBackColor = false;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
             // 
             // label2
             // 
@@ -161,70 +151,92 @@ namespace Mens_Beauty_Center
             this.label4.TabIndex = 5;
             this.label4.Text = ":سعر البكج";
             // 
-            // textBox1
+            // PackageNameTxt
             // 
-            this.textBox1.Location = new System.Drawing.Point(43, 140);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(195, 20);
-            this.textBox1.TabIndex = 6;
+            this.PackageNameTxt.Location = new System.Drawing.Point(43, 140);
+            this.PackageNameTxt.Name = "PackageNameTxt";
+            this.PackageNameTxt.Size = new System.Drawing.Size(195, 20);
+            this.PackageNameTxt.TabIndex = 6;
             // 
-            // richTextBox1
+            // DescriptionTxt
             // 
-            this.richTextBox1.Location = new System.Drawing.Point(43, 193);
-            this.richTextBox1.Name = "richTextBox1";
-            this.richTextBox1.Size = new System.Drawing.Size(195, 151);
-            this.richTextBox1.TabIndex = 7;
-            this.richTextBox1.Text = "";
+            this.DescriptionTxt.Location = new System.Drawing.Point(43, 193);
+            this.DescriptionTxt.Name = "DescriptionTxt";
+            this.DescriptionTxt.Size = new System.Drawing.Size(195, 151);
+            this.DescriptionTxt.TabIndex = 7;
+            this.DescriptionTxt.Text = "";
             // 
-            // textBox2
+            // PriceTxt
             // 
-            this.textBox2.Location = new System.Drawing.Point(43, 367);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(194, 20);
-            this.textBox2.TabIndex = 8;
+            this.PriceTxt.Location = new System.Drawing.Point(43, 367);
+            this.PriceTxt.Name = "PriceTxt";
+            this.PriceTxt.Size = new System.Drawing.Size(194, 20);
+            this.PriceTxt.TabIndex = 8;
+            // 
+            // Package_ID
+            // 
+            this.Package_ID.HeaderText = "الرقم";
+            this.Package_ID.Name = "Package_ID";
+            // 
+            // Package_Name
+            // 
+            this.Package_Name.HeaderText = "الاسم";
+            this.Package_Name.Name = "Package_Name";
+            // 
+            // PackageDescription
+            // 
+            this.PackageDescription.HeaderText = "المحتويات";
+            this.PackageDescription.Name = "PackageDescription";
+            // 
+            // PackagePrice
+            // 
+            this.PackagePrice.HeaderText = "السعر";
+            this.PackagePrice.Name = "PackagePrice";
             // 
             // PackageForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(869, 484);
-            this.Controls.Add(this.textBox2);
-            this.Controls.Add(this.richTextBox1);
-            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.PriceTxt);
+            this.Controls.Add(this.DescriptionTxt);
+            this.Controls.Add(this.PackageNameTxt);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.UpdateBtn);
+            this.Controls.Add(this.AddBtn);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.dataGridView1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "PackageForm";
             this.Text = "PckageForm";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.LoginForm_FormClosing);
+            this.Load += new System.EventHandler(this.PackageForm_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
-            this.FormClosing += new FormClosingEventHandler(LoginForm_FormClosing);
 
         }
 
         #endregion
 
         private System.Windows.Forms.DataGridView dataGridView1;
-        private System.Windows.Forms.DataGridViewTextBoxColumn PackageName;
-        private System.Windows.Forms.DataGridViewTextBoxColumn Description;
-        private System.Windows.Forms.DataGridViewTextBoxColumn TotalPrice;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button AddBtn;
+        private System.Windows.Forms.Button UpdateBtn;
+        private System.Windows.Forms.Button DeleteBtn;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.RichTextBox richTextBox1;
-        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox PackageNameTxt;
+        private System.Windows.Forms.RichTextBox DescriptionTxt;
+        private System.Windows.Forms.TextBox PriceTxt;
+        private DataGridViewTextBoxColumn Package_ID;
+        private DataGridViewTextBoxColumn Package_Name;
+        private DataGridViewTextBoxColumn PackageDescription;
+        private DataGridViewTextBoxColumn PackagePrice;
     }
 }
--- Project/Mens_Beauty_Center/Mens_Beauty_Center/PackageForm.cs
+++ Project/Mens_Beauty_Center/Mens_Beauty_Center/PackageForm.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Contexts;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -24,5 +25,204 @@ namespace Mens_Beauty_Center
                 e.Cancel = true;
             }
         }
+
+        Mens_Beauty_Center_DBEntities context = new Mens_Beauty_Center_DBEntities();
+
+        #region load data
+        private void LoadPackages()
+        {
+            try
+            {
+                var packages = context.Packages.Select(p => new
+                {
+                    p.ID,
+                    p.Name,
+                    p.Description,
+                    p.TotalAmount
+                }).ToList();
+
+                dataGridView1.Rows.Clear();
+                foreach (var package in packages)
+                {
+                    dataGridView1.Rows.Add(package.ID, package.Name, package.Description, package.TotalAmount);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("An error occurred while loading the packages: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void PackageForm_Load(object sender, EventArgs e)
+        {
+            LoadPackages();
+        }
+        #endregion
+
+        #region add Package
+        private void AddBtn_Click(object sender, EventArgs e)
+        {
+            string packageName = PackageNameTxt.Text;
+            string description = DescriptionTxt.Text;
+            decimal price;
+            if (decimal.TryParse(PriceTxt.Text, out price))
+            {
+                try
+                {
+                    var result = context.SP_AddPackage(packageName, description, price);
+                    if (result > 0)
+                    {
+                        LoadPackages(); // Reload the DataGridView
+
+                        PackageNameTxt.Clear();
+                        DescriptionTxt.Clear();
+                        PriceTxt.Clear();
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to add the package. Please try again.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please enter a valid price.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        private void LoadPackages2()
+        {
+            dataGridView1.Rows.Clear();
+            var packages = context.Packages.ToList(); // Assuming 'Packages' is the table name
+
+            foreach (var package in packages)
+            {
+                DataGridViewRow row = new DataGridViewRow();
+                row.CreateCells(dataGridView1);
+                row.Cells[0].Value = package.Name;
+                row.Cells[1].Value = package.Description;
+                row.Cells[2].Value = package.TotalAmount.ToString("C2");
+                dataGridView1.Rows.Add(row);
+            }
+        }
+
+        #endregion
+
+        #region delete Package
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DialogResult confirmResult = MessageBox.Show("هل انت متاكد من مسح هذه البكج؟",
+                                                             "Confirm Delete",
+                                                             MessageBoxButtons.YesNo,
+                                                             MessageBoxIcon.Warning);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                    int packageId = Convert.ToInt32(selectedRow.Cells["Package_ID"].Value);
+
+                    using (var context = new Mens_Beauty_Center_DBEntities())
+                    {
+                        var result = context.SP_DeletePackage(packageId);
+
+                        if (result != null)
+                        {
+                            dataGridView1.Rows.Remove(selectedRow);
+
+                            MessageBox.Show("تم المسح بنجاح");
+                        }
+                        else
+                        {
+                            MessageBox.Show("Failed to delete the package.");
+                        }
+                    }
+                }
+                // If the user canceled the deletion
+                else
+                {
+                    MessageBox.Show("Deletion canceled.");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a package to delete.");
+            }
+        }
+        #endregion
+
+        #region Update Package
+        private void UpdateBtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count > 0)
+            {
+                DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+
+                int packageId = Convert.ToInt32(selectedRow.Cells["Package_ID"].Value);
+                string currentName = selectedRow.Cells["Package_Name"].Value.ToString();
+                string currentDescription = selectedRow.Cells["PackageDescription"].Value.ToString();
+                decimal currentTotalAmount = Convert.ToDecimal(selectedRow.Cells["PackagePrice"].Value);
+                PackageNameTxt.Text = currentName;
+                DescriptionTxt.Text = currentDescription;
+                PriceTxt.Text = currentTotalAmount.ToString("F2");
+                DialogResult confirmResult = MessageBox.Show("هل انت متاكد من تحديث هذه البكج؟",
+                                                             "Confirm Update",
+                                                             MessageBoxButtons.YesNo,
+                                                             MessageBoxIcon.Warning);
+
+                if (confirmResult == DialogResult.Yes)
+                {
+                    string newName = PackageNameTxt.Text;
+                    string newDescription = DescriptionTxt.Text;
+                    decimal newTotalAmount;
+
+                    if (decimal.TryParse(PriceTxt.Text, out newTotalAmount))
+                    {
+                        try
+                        {
+                            using (var context = new Mens_Beauty_Center_DBEntities())
+                            {
+                                var result = context.SP_UpdatePackage(packageId, newName, newDescription, newTotalAmount);
+
+                                if (result != null && result.Any())
+                                {
+                                    selectedRow.Cells["Package_Name"].Value = newName;
+                                    selectedRow.Cells["PackageDescription"].Value = newDescription;
+                                    selectedRow.Cells["PackagePrice"].Value = newTotalAmount.ToString("C2");
+
+                                    MessageBox.Show("تم التحديث بنجاح");
+
+                                    PackageNameTxt.Clear();
+                                    DescriptionTxt.Clear();
+                                    PriceTxt.Clear();
+                                }
+                                else
+                                {
+                                    MessageBox.Show("Failed to update the package.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                                }
+                            }
+                        }
+                        catch (Exception ex)
+                        {
+                            MessageBox.Show("An error occurred: " + ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                        }
+                    }
+                    else
+                    {
+                        MessageBox.Show("Please enter a valid price.", "Invalid Input", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Please select a package to update.", "No Selection", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+            }
+        }
+        #endregion
+
     }
 }
