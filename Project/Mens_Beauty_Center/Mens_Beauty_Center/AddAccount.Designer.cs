df020acde6a99da64299ff1a3f21b9f1f9a9716b
Faresahmed9 <faresfouad5550@gmail.com>
2024-08-22 23:41:23 +0200

    Add Account

---
+++ Project/Mens_Beauty_Center/Mens_Beauty_Center/AddAccount.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace Mens_Beauty_Center
+{
+    partial class AddAccount
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.NameManagerTxt = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.PasswordManagerTxt = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.UserName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.EmployeeName = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DeleteBtn = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Tahoma", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(438, 78);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(200, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "اضافه اكونت للمدير";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(606, 229);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(156, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":اسم المستخدم";
+            // 
+            // NameManagerTxt
+            // 
+            this.NameManagerTxt.Location = new System.Drawing.Point(363, 229);
+            this.NameManagerTxt.Name = "NameManagerTxt";
+            this.NameManagerTxt.Size = new System.Drawing.Size(163, 20);
+            this.NameManagerTxt.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(532, 288);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(230, 23);
+            this.label3.TabIndex = 1;
+            this.label3.Text = ":الرقم السرى للمستخدم";
+            // 
+            // PasswordManagerTxt
+            // 
+            this.PasswordManagerTxt.Location = new System.Drawing.Point(363, 293);
+            this.PasswordManagerTxt.Name = "PasswordManagerTxt";
+            this.PasswordManagerTxt.PasswordChar = '*';
+            this.PasswordManagerTxt.Size = new System.Drawing.Size(163, 20);
+            this.PasswordManagerTxt.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(363, 159);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(163, 21);
+            this.comboBox1.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(671, 154);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(91, 23);
+            this.label4.TabIndex = 1;
+            this.label4.Text = ":المديرين";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Green;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.button1.Location = new System.Drawing.Point(585, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 34);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "اضافه";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.UserName,
+            this.EmployeeName});
+            this.dataGridView1.Location = new System.Drawing.Point(3, 78);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(305, 301);
+            this.dataGridView1.TabIndex = 5;
+            // 
+            // UserName
+            // 
+            this.UserName.HeaderText = "اسم المستخدم";
+            this.UserName.Name = "UserName";
+            // 
+            // EmployeeName
+            // 
+            this.EmployeeName.HeaderText = "اسم المدير";
+            this.EmployeeName.Name = "EmployeeName";
+            // 
+            // DeleteBtn
+            // 
+            this.DeleteBtn.BackColor = System.Drawing.Color.DarkRed;
+            this.DeleteBtn.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.DeleteBtn.FlatAppearance.BorderSize = 0;
+            this.DeleteBtn.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.DeleteBtn.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.DeleteBtn.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.DeleteBtn.Location = new System.Drawing.Point(465, 352);
+            this.DeleteBtn.Name = "DeleteBtn";
+            this.DeleteBtn.Size = new System.Drawing.Size(75, 33);
+            this.DeleteBtn.TabIndex = 6;
+            this.DeleteBtn.Text = "مسح";
+            this.DeleteBtn.UseVisualStyleBackColor = false;
+            this.DeleteBtn.Click += new System.EventHandler(this.DeleteBtn_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Tahoma", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(31, 27);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(268, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "عرض جميع الاكونتات المتاحه";
+            // 
+            // AddAccount
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 398);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.DeleteBtn);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.PasswordManagerTxt);
+            this.Controls.Add(this.NameManagerTxt);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "AddAccount";
+            this.Text = "AddAccount";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.AddAccount_FormClosing);
+            this.Load += new System.EventHandler(this.AddAccount_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox NameManagerTxt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox PasswordManagerTxt;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn UserName;
+        private System.Windows.Forms.DataGridViewTextBoxColumn EmployeeName;
+        private System.Windows.Forms.Button DeleteBtn;
+        private System.Windows.Forms.Label label5;
+    }
+}
---
+++ Project/Mens_Beauty_Center/Mens_Beauty_Center/AddAccount.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.Entity;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mens_Beauty_Center
+{
+    public partial class AddAccount : Form
+    {
+        public AddAccount()
+        {
+            InitializeComponent();
+        }
+        private void AddAccount_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("هل انت متاكد من غلق هذه الصفحه؟", "Confirm Close", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+        #region load Data
+        private void LoadData()
+        {
+            try
+            {
+                using (var context = new Mens_Beauty_Center_DBEntities()) // Replace with your actual DbContext
+                {
+                    var employees = context.Employees
+                                           .Where(emp => emp.Type == true) // Filter where Type is true
+                                           .Select(emp => new
+                                           {
+                                               EmployeeID = emp.NationalID, // Assuming NationalID is the unique ID
+                                               EmployeeName = emp.FirstName + " " + emp.LastName
+                                           })
+                                           .ToList();
+
+                    comboBox1.DataSource = employees;
+                    comboBox1.DisplayMember = "EmployeeName"; // Display full name
+                    comboBox1.ValueMember = "EmployeeID";     // Store NationalID
+                    comboBox1.SelectedIndex = -1; // Optionally, set it to no selection
+                }
+                // show data in grid view  
+                using (var context = new Mens_Beauty_Center_DBEntities()) // Replace with your actual DbContext
+                {
+                    var accounts = (from acc in context.Accounts
+                                    join emp in context.Employees
+                                    on acc.EmployeeID equals emp.NationalID
+                                    where acc.EmployeeID != null
+                                    select new
+                                    {
+                                        acc.UserName,
+                                        EmployeeName = emp.FirstName + " " + emp.LastName // Assuming 'Name' is the property in the Employee table
+                                    }).ToList();
+
+                    dataGridView1.Rows.Clear();
+                    foreach (var account in accounts)
+                    {
+                        dataGridView1.Rows.Add(account.UserName, account.EmployeeName);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred while loading employees: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+
+
+        }
+        private void AddAccount_Load(object sender, EventArgs e)
+        {
+            LoadData();
+        }
+        #endregion
+
+        #region Add Account
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string userName = NameManagerTxt.Text;
+            string userPassword = PasswordManagerTxt.Text;
+            var selectedEmployeeId = comboBox1.SelectedValue?.ToString(); 
+
+            if (string.IsNullOrEmpty(userName) || string.IsNullOrEmpty(userPassword) || string.IsNullOrEmpty(selectedEmployeeId))
+            {
+                MessageBox.Show("قم باملاء البيانات .", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                using (var context = new Mens_Beauty_Center_DBEntities()) 
+                {
+                    var existingAccount = context.Accounts
+                                        .FirstOrDefault(emp => emp.EmployeeID == selectedEmployeeId);
+
+                    if (existingAccount != null)
+                    {
+                        MessageBox.Show("هذا الاكونت موجود بالفعل لهذا المدير", "Error", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                        NameManagerTxt.Clear();
+                        PasswordManagerTxt.Clear();
+                        return;
+                    }
+
+                    // If the account does not exist, proceed to add it
+                    int result = context.SP_CreateManagerAccount(userName, userPassword, selectedEmployeeId);
+
+                    if (result > 0)
+                    {
+                        MessageBox.Show("تمت اضافه الاكونت بنجاح.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                        NameManagerTxt.Clear();
+                        PasswordManagerTxt.Clear();
+                        LoadData();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Failed to add manager account.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        #endregion
+
+        #region DeleteAccount
+        private void DeleteBtn_Click(object sender, EventArgs e)
+        {
+            if (dataGridView1.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("يرجى تحديد صف لحذفه.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+
+            DataGridViewRow selectedRow = dataGridView1.SelectedRows[0];
+            string userName = selectedRow.Cells["UserName"].Value.ToString();
+
+
+            DialogResult result = MessageBox.Show("هل أنت متأكد من حذف هذا الحساب؟", "Confirm Delete", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                return;
+            }
+
+            try
+            {
+                using (var context = new Mens_Beauty_Center_DBEntities())
+                {
+                    int affectedRows = context.SP_DeleteManagerAccount(userName);
+
+                    if (affectedRows > 0)
+                    {
+                        dataGridView1.Rows.Remove(selectedRow);
+                        MessageBox.Show("تم حذف الحساب بنجاح.", "Success", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                    }
+                    else
+                    {
+                        MessageBox.Show("فشل حذف الحساب.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"حدث خطأ: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+    }
+}
