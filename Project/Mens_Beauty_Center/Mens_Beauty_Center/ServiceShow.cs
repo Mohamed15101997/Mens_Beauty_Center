f0227112559597689f605cca287aa2e15b719ae4
Eslam Waheed <waheedeslam37@gmail.com>
2024-08-23 15:03:04 +0300

    SeviceShow and Taken Commit

---
+++ Project/Mens_Beauty_Center/Mens_Beauty_Center/ServiceShow.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Mens_Beauty_Center
+{
+    public partial class ServiceShow : Form
+    {
+        Mens_Beauty_Center_DBEntities context = new Mens_Beauty_Center_DBEntities();
+        
+        public ServiceShow()
+        {
+            InitializeComponent();
+            fillTheDataGridView();
+            dataGridViewService.Click += dataGridViewService_Click;
+        }
+        
+        private void ServiceShow_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Add_btn_Click_1(object sender, EventArgs e)
+        {
+            string _servicename = textBoxServices.Text.ToString();
+            if (_servicename.Length == 0)
+            {
+                MessageBox.Show("رجاءا اكتب اسم الخدمة اولاً");
+                return;
+            }
+            if (!(int.TryParse(textBoxPrice.Text.ToString(), out int _price)))
+            {
+                MessageBox.Show("رجاءا اكتب سعر الخدمة علي هيئة رقم صحيح");
+                return;
+            }
+            DialogResult result = MessageBox.Show(@"هل انت متأكد انك تريد إضافة هذه الخدمة؟", "تأكيد إضافة", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                context.SP_AddService(_servicename, _price);
+                context.SaveChanges();
+                fillTheDataGridView();
+                textBoxPrice.Text = string.Empty;
+                textBoxServices.Text = string.Empty;
+            }
+        }
+
+        private void dataGridViewService_Click(object sender, EventArgs e)
+        {
+            int postion = dataGridViewService.CurrentRow.Index;
+            textBoxServices.Text = dataGridViewService.Rows[postion].Cells[1].Value.ToString();
+            textBoxPrice.Text = dataGridViewService.Rows[postion].Cells[0].Value.ToString();
+
+        }
+
+        private void Edite_btn_Click_1(object sender, EventArgs e)
+        {
+            var selectedRow = dataGridViewService.SelectedRows[0];
+
+            var id = (int)selectedRow.Cells[2].Value;
+            var findId = context.Services.FirstOrDefault(x => x.ID == id);
+            if (findId is null)
+            {
+                MessageBox.Show("رجاءا ادخل خدمة موجودة بالفعل");
+                return;
+            }
+            string _servicename = textBoxServices.Text.ToString();
+            if (_servicename.Length == 0)
+            {
+                MessageBox.Show("رجاءا اكتب اسم الخدمة اولاً");
+                return;
+            }
+            if (!(int.TryParse(textBoxPrice.Text.ToString(), out int _price)))
+            {
+                MessageBox.Show("رجاءا اكتب سعر الخدمة علي هيئة رقم صحيح");
+                return;
+            }
+
+
+            DialogResult result = MessageBox.Show($"هل انت متأكد انك تريد التعديل علي الخدمة رقم {id} ؟", "تأكيد تعديل", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.Yes)
+            {
+                context.SP_UpdateService(id, _servicename, _price);
+                //context.SaveChanges();
+                fillTheDataGridView();
+                textBoxPrice.Text = string.Empty;
+                textBoxServices.Text = string.Empty;
+            }
+        }
+
+        private void ServiceShow_Load_1(object sender, EventArgs e)
+        {
+            
+        }
+        private void ServiceShow_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("هل انت متاكد من اغلاق الصفحة", "Closing", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.No)
+                e.Cancel = true;
+        }
+
+        private void fillTheDataGridView()
+        {
+            var allServices = context.Services.ToList();
+            dataGridViewService.Rows.Clear();
+            foreach (var service in allServices) {
+                dataGridViewService.Rows.Add(service.Price, service.ServiceName, service.ID);
+            }
+        }
+
+        private void ServiceShow_FormClosing_1(object sender, FormClosingEventArgs e)
+        {
+            DialogResult result = MessageBox.Show("هل انت متاكد من اغلاق الصفحة", "Closing", MessageBoxButtons.YesNo, MessageBoxIcon.Question, MessageBoxDefaultButton.Button2);
+            if (result == DialogResult.No)
+                e.Cancel = true;
+        }
+    }
+}
---
+++ Project/Mens_Beauty_Center/Mens_Beauty_Center/ServicesTaken.Designer.cs
@@ -0,0 +1,260 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace Mens_Beauty_Center
+{
+    partial class ServicesTaken
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.Search_btn = new System.Windows.Forms.Button();
+            this.comboBoxTheEmployees = new System.Windows.Forms.ComboBox();
+            this.dateTimePickerTo = new System.Windows.Forms.DateTimePicker();
+            this.dateTimePickerFrom = new System.Windows.Forms.DateTimePicker();
+            this.checkBoxPackage = new System.Windows.Forms.CheckBox();
+            this.checkBoxService = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dataGridViewForTheWorkOfTheEmployees = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewForTheWorkOfTheEmployees)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(429, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 48);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "شغل الموظفين";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.Search_btn);
+            this.groupBox1.Controls.Add(this.comboBoxTheEmployees);
+            this.groupBox1.Controls.Add(this.dateTimePickerTo);
+            this.groupBox1.Controls.Add(this.dateTimePickerFrom);
+            this.groupBox1.Controls.Add(this.checkBoxPackage);
+            this.groupBox1.Controls.Add(this.checkBoxService);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Location = new System.Drawing.Point(12, 77);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(361, 337);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            // 
+            // Search_btn
+            // 
+            this.Search_btn.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Search_btn.Location = new System.Drawing.Point(6, 274);
+            this.Search_btn.Name = "Search_btn";
+            this.Search_btn.Size = new System.Drawing.Size(347, 41);
+            this.Search_btn.TabIndex = 4;
+            this.Search_btn.Text = "بحث";
+            this.Search_btn.UseVisualStyleBackColor = true;
+            this.Search_btn.Click += new System.EventHandler(this.Search_btn_Click);
+            // 
+            // comboBoxTheEmployees
+            // 
+            this.comboBoxTheEmployees.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxTheEmployees.FormattingEnabled = true;
+            this.comboBoxTheEmployees.Location = new System.Drawing.Point(6, 149);
+            this.comboBoxTheEmployees.Name = "comboBoxTheEmployees";
+            this.comboBoxTheEmployees.Size = new System.Drawing.Size(244, 28);
+            this.comboBoxTheEmployees.TabIndex = 4;
+            // 
+            // dateTimePickerTo
+            // 
+            this.dateTimePickerTo.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerTo.Location = new System.Drawing.Point(6, 80);
+            this.dateTimePickerTo.Name = "dateTimePickerTo";
+            this.dateTimePickerTo.Size = new System.Drawing.Size(292, 26);
+            this.dateTimePickerTo.TabIndex = 3;
+            this.dateTimePickerTo.ValueChanged += new System.EventHandler(this.dateTimePickerTo_ValueChanged);
+            // 
+            // dateTimePickerFrom
+            // 
+            this.dateTimePickerFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePickerFrom.Location = new System.Drawing.Point(6, 32);
+            this.dateTimePickerFrom.Name = "dateTimePickerFrom";
+            this.dateTimePickerFrom.Size = new System.Drawing.Size(292, 26);
+            this.dateTimePickerFrom.TabIndex = 3;
+            this.dateTimePickerFrom.ValueChanged += new System.EventHandler(this.dateTimePickerFrom_ValueChanged);
+            // 
+            // checkBoxPackage
+            // 
+            this.checkBoxPackage.AutoSize = true;
+            this.checkBoxPackage.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxPackage.Location = new System.Drawing.Point(209, 208);
+            this.checkBoxPackage.Name = "checkBoxPackage";
+            this.checkBoxPackage.Size = new System.Drawing.Size(89, 40);
+            this.checkBoxPackage.TabIndex = 2;
+            this.checkBoxPackage.Text = "البكج";
+            this.checkBoxPackage.UseVisualStyleBackColor = true;
+            // 
+            // checkBoxService
+            // 
+            this.checkBoxService.AutoSize = true;
+            this.checkBoxService.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBoxService.Location = new System.Drawing.Point(17, 208);
+            this.checkBoxService.Name = "checkBoxService";
+            this.checkBoxService.Size = new System.Drawing.Size(104, 40);
+            this.checkBoxService.TabIndex = 2;
+            this.checkBoxService.Text = "الخدمة";
+            this.checkBoxService.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(260, 141);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(99, 36);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "الموظف";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(311, 74);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(48, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "إلي";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(310, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 36);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "من";
+            // 
+            // dataGridViewForTheWorkOfTheEmployees
+            // 
+            this.dataGridViewForTheWorkOfTheEmployees.AllowUserToAddRows = false;
+            this.dataGridViewForTheWorkOfTheEmployees.AllowUserToDeleteRows = false;
+            this.dataGridViewForTheWorkOfTheEmployees.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridViewForTheWorkOfTheEmployees.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridViewForTheWorkOfTheEmployees.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewForTheWorkOfTheEmployees.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2,
+            this.Column3,
+            this.Column4});
+            this.dataGridViewForTheWorkOfTheEmployees.Location = new System.Drawing.Point(400, 77);
+            this.dataGridViewForTheWorkOfTheEmployees.Name = "dataGridViewForTheWorkOfTheEmployees";
+            this.dataGridViewForTheWorkOfTheEmployees.ReadOnly = true;
+            this.dataGridViewForTheWorkOfTheEmployees.RowHeadersWidth = 51;
+            this.dataGridViewForTheWorkOfTheEmployees.RowTemplate.Height = 24;
+            this.dataGridViewForTheWorkOfTheEmployees.Size = new System.Drawing.Size(726, 337);
+            this.dataGridViewForTheWorkOfTheEmployees.TabIndex = 2;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "الموظف";
+            this.Column1.MinimumWidth = 6;
+            this.Column1.Name = "Column1";
+            this.Column1.ReadOnly = true;
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "العميل";
+            this.Column2.MinimumWidth = 6;
+            this.Column2.Name = "Column2";
+            this.Column2.ReadOnly = true;
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "الخدمة/البكج";
+            this.Column3.MinimumWidth = 6;
+            this.Column3.Name = "Column3";
+            this.Column3.ReadOnly = true;
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "سعر الخدمة/البكج";
+            this.Column4.MinimumWidth = 6;
+            this.Column4.Name = "Column4";
+            this.Column4.ReadOnly = true;
+            // 
+            // ServicesTaken
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1138, 432);
+            this.Controls.Add(this.dataGridViewForTheWorkOfTheEmployees);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.MaximizeBox = false;
+            this.Name = "ServicesTaken";
+            this.Text = "ServicesTaken";
+            this.Load += new System.EventHandler(this.ServicesTaken_Load_1);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewForTheWorkOfTheEmployees)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.CheckBox checkBoxService;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePickerFrom;
+        private System.Windows.Forms.Button Search_btn;
+        private System.Windows.Forms.ComboBox comboBoxTheEmployees;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.DataGridView dataGridViewForTheWorkOfTheEmployees;
+        private System.Windows.Forms.DateTimePicker dateTimePickerTo;
+        private System.Windows.Forms.CheckBox checkBoxPackage;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+    }
+}
